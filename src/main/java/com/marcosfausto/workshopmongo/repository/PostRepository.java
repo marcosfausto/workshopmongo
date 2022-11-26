c9538fa1929b43f6bb59b12be6802d1649241073
Marcos <marcos_fausto@yahoo.com.br>
2022-11-26 22:02:53 +0000

    Consulta simples com @query

--- src/main/java/com/marcosfausto/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/marcosfausto/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.marcosfausto.workshopmongo.repository;
 import com.marcosfausto.workshopmongo.domain.Post;
 import com.marcosfausto.workshopmongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +11,8 @@ import java.util.List;
 @Repository
 public interface PostRepository extends MongoRepository<Post,String> {
 
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }") // ?0 = primeiro parametro passado, $options: 'i' = case insensitive
+    List<Post> searchTitle(String text);
     List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
